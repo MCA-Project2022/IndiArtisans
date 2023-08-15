73eda5b0eee3371112f08a288e83f9fafc6c11d9
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2023-08-15 15:48:51 +0530

    Add Filter component.

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -4,6 +4,7 @@ import getProducts from "@/actions/get-products";
 import getSizes from "@/actions/get-sizes";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
+import Filter from "./components/filter";
 
 export const revalidate = 0;
 
@@ -27,7 +28,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
   });
 
   const sizes = await getSizes();
-  const color = await getColors();
+  const colors = await getColors();
   const category = await getCategory(params.categoryId);
 
   return (
@@ -36,6 +37,22 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <Billboard
           data={category.billboard}
         />
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            <div className="hidden lg:block">
+              <Filter
+                valueKey="sizeId"
+                name="Sizes"
+                data={sizes}
+              />
+              <Filter
+                valueKey="colorId"
+                name="Colors"
+                data={colors}
+              />
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
